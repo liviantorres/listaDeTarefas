5debd70a7399c417b12d155583f38ae3362358dd
Livian Torres <liviantorres@alu.ufc.br>
2024-07-01 21:52:30 -0300

    Buscando as tarefas no banco

--- index.js
+++ index.js
@@ -7,3 +7,26 @@ function fecharModal(){
     overlay.classList.remove("active")
     criarTarefa.classList.remove("active")
 }
+
+function buscarTarefas(){
+    fetch("http://localhost:3000/tarefas")
+    .then(res => res.json())
+    .then(res =>{
+        inserirTarefas(res);
+    })
+}
+buscarTarefas();
+
+function inserirTarefas(listaDeTarefas){
+    if(listaDeTarefas.length > 0){
+        listaDeTarefas.map(tarefa => {
+            lista.innerHTML += `
+             <li>
+                    <h5>${tarefa.titulo}</h5>
+                    <p>${tarefa.descricao}</p>
+                    <div class="actions"><box-icon name='trash' ></box-icon></div>
+                </li>
+            `
+        })
+    }
+}
